089ff1ba0d71102ccebbbab49a029b3856e0a3d4
Thien Nguyen <quangthien27@gmail.com>
2018-06-06 16:21:59 +1000

    update fur

--- js/furniture.js
+++ js/furniture.js
@@ -1,17 +1,15 @@
 function loadFurniture() {
-    // ADD MODELS
-
     //BEDROOM 1
     getModel("Bed02.obj", 0, 0, 0, "Bed02.mtl", 17, 45, 5, -75);
     getModel("wardrobe_OBJ.obj", 0, 4.70, 0, "Bed02.mtl", 10, 65, 15, -60);
     getModel("Bar_table.obj", 0, 4.70, 0, "Bar_table.mtl", 15, 30, 5, -71);
     getModel("treasure_chest.obj", 0, 0, 0, "treasure_chest.mtl", 15, 12, 5, -15);
-    getModel("shelf.obj", 0, 0, 0, "SofaCollection.mtl", 1.3, 10, 15, -11); // Bigmax_White_OBJ.obj
+    getModel("shelf.obj", 0, 0, 0, "SofaCollection.mtl", 1.3, 10, 15, -11);
     getModel("Bigmax_White_OBJ.obj", 0, 0, 0, "Bigmax_White_OBJ.mtl", 7, 15, 5, -28);
     getModel("D_Plant.obj", 4.7, 0, 0, "D_Plant.mtl", 10, 60, 10, -28);
 
     //KITCHEN
-    getModel("modern_table_and_chairs_obj.obj", 0, 0, 0, "modern_table_and_chairs_obj.mtl", 12, 45, 5, 50); // Kitchen_3_Wardrobe.obj
+    getModel("modern_table_and_chairs_obj.obj", 0, 0, 0, "modern_table_and_chairs_obj.mtl", 12, 45, 5, 50);
     getModel("Kitchen_3_Wardrobe.obj", 0, 4.7, 0, "Kitchen_3_Wardrobe.mtl", 2, 65, 5, 60);
 
     //BEDROOM 2
@@ -22,21 +20,20 @@ function loadFurniture() {
     //LIVING ROOM
     getModel("shelf.obj", 0, 0, 0, "SofaCollection.mtl", 2, -38, 13, 91);
     getModel("SofaCollection.obj", 0, 7.8, 0, "SofaCollection.mtl", 10, -40, 5.5, 25);
-    getModel( "tv.obj" , 14.1, 3.15, 4.7, "treasure_chest.mtl", 13, 13, 5.5, 25);
-    //getModel( "Cocktail_Table.obj" , 0, 0, 0, "SofaCollection.mtl", 18, -20, 5.5, 25); // sofa table without texture
-    getModel( "Coffeet2.obj" , 4.7, 0, 4.7, "Coffeet2.mtl", 24, -10, 5, 82);
-    getModel( "sofa.obj" , 0, 0, 0, "sofa.mtl", 24, -5, 8.5, 60);
-    getModel("mus001.obj", 4.7, 0, 0, "mus001.mtl", 6, -30, 5, 68); //piano
-    getModel("painting.obj", 0, 0, 0, "painting.mtl", 25, -9, 9, 11.4); //Painting
+    getModel("tv.obj", 14.1, 3.15, 4.7, "treasure_chest.mtl", 13, 13, 5.5, 25);
+    getModel("Coffeet2.obj", 4.7, 0, 4.7, "Coffeet2.mtl", 24, -10, 5, 82);
+    getModel("sofa.obj", 0, 0, 0, "sofa.mtl", 24, -5, 8.5, 60);
+    getModel("mus001.obj", 4.7, 0, 0, "mus001.mtl", 6, -30, 5, 68);
+    getModel("painting.obj", 0, 0, 0, "painting.mtl", 25, -9, 9, 11.4);
 
     //CORRIDOR
-    getModel("Coat-Tree.obj", 4.7, 0, 0, "Coat-Tree.mtl", 26, -40, 7, -5); // D_Plant.obj
-
-
+    getModel("Coat-Tree.obj", 4.7, 0, 0, "Coat-Tree.mtl", 26, -40, 7, -5);
 
     //BATHROOM
-    getModel( "toilet.obj", 0, 9.4, 0, "toilet.mtl", 1.5, 63, 5, 0);
+    getModel("toilet.obj", 0, 9.4, 0, "toilet.mtl", 1.5, 63, 5, 0);
 
+    //ADAPT DRAG AND DROP
+    adaptDragAndDrop(objects);
 }
 
 function getModel(objFileName, xRotate, yRotate, zRotate, mtlFileName, objectSize, xCor, yCor, zCor) {
@@ -44,6 +41,7 @@ function getModel(objFileName, xRotate, yRotate, zRotate, mtlFileName, objectSiz
     var mtlLoader = new THREE.MTLLoader();
     mtlLoader.setTexturePath("models/");
     mtlLoader.setPath("models/");
+    
     var objLoader = new THREE.OBJLoader();
     objLoader.setPath("models/");
 
@@ -66,12 +64,11 @@ function getModel(objFileName, xRotate, yRotate, zRotate, mtlFileName, objectSiz
                     SizeBB = mygeometry.boundingBox.getSize();
                     child.castShadow = true;
                     child.receiveShadow = true;
+                    objects.push(child);
                 }
             });
 
             scene.add(mesh);
-            objects.push(mesh);
-            adaptDragAndDrop(objects);
 
             var sca = new THREE.Matrix4();
             var tra = new THREE.Matrix4();
