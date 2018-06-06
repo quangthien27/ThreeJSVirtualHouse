8e03b6b973c4e00bf82f9600036ac3b46b9c59c4
Thien Nguyen <quangthien27@gmail.com>
2018-06-06 11:14:50 +1000

    update

--- js/main.js
+++ js/main.js
@@ -4,6 +4,12 @@
   var scene = new THREE.Scene();
 
   var sky, sunSphere, objects = [];
+  var sunCircling = true;
+  var gui = new dat.GUI();
+
+  var effectController = {
+      sunSpeed: .5,
+  };
 
   //create the perspective camera
   //for parameters see https://threejs.org/docs/#api/cameras/PerspectiveCamera
@@ -149,56 +155,17 @@
 
       /// GUI
 
-      // var effectController = {
-      //     turbidity: 10,
-      //     rayleigh: 2,
-      //     mieCoefficient: 0.005,
-      //     mieDirectionalG: 0.8,
-      //     luminance: 1,
-      //     inclination: 0.49, // elevation / inclination
-      //     azimuth: 0.25, // Facing front,
-      //     sun: !true
-      // };
-      //
-      // var distance = 400000;
-      //
-      // function guiChanged() {
-      //
-      //     var uniforms = sky.material.uniforms;
-      //     uniforms.turbidity.value = effectController.turbidity;
-      //     uniforms.rayleigh.value = effectController.rayleigh;
-      //     uniforms.luminance.value = effectController.luminance;
-      //     uniforms.mieCoefficient.value = effectController.mieCoefficient;
-      //     uniforms.mieDirectionalG.value = effectController.mieDirectionalG;
-      //
-      //     var theta = Math.PI * ( effectController.inclination - 0.5 );
-      //     var phi = 2 * Math.PI * ( effectController.azimuth - 0.5 );
-      //
-      //     sunSphere.position.x = distance * Math.cos(phi);
-      //     sunSphere.position.y = distance * Math.sin(phi) * Math.sin(theta);
-      //     sunSphere.position.z = distance * Math.sin(phi) * Math.cos(theta);
-      //
-      //     sunSphere.visible = effectController.sun;
-      //
-      //     uniforms.sunPosition.value.copy(sunSphere.position);
-      //
-      //     // renderer.render(scene, camera);
-      //
-      // }
-      //
-      // var gui = new dat.GUI();
-      //
-      // gui.add(effectController, "turbidity", 1.0, 20.0, 0.1).onChange(guiChanged);
-      // gui.add(effectController, "rayleigh", 0.0, 4, 0.001).onChange(guiChanged);
-      // gui.add(effectController, "mieCoefficient", 0.0, 0.1, 0.001).onChange(guiChanged);
-      // gui.add(effectController, "mieDirectionalG", 0.0, 1, 0.001).onChange(guiChanged);
-      // gui.add(effectController, "luminance", 0.0, 2).onChange(guiChanged);
-      // gui.add(effectController, "inclination", 0, 1, 0.0001).onChange(guiChanged);
-      // gui.add(effectController, "azimuth", 0, 1, 0.0001).onChange(guiChanged);
-      // gui.add(effectController, "sun").onChange(guiChanged);
-      //
-      // guiChanged();
   };
+
+  function guiChanged() {
+
+
+  }
+
+  gui.add(effectController, "sunSpeed", 0.05, 7.0, 0.05).onChange(guiChanged);
+
+  guiChanged();
+
 
   var adaptDragAndDrop = function () {
       var geometry = new THREE.BoxBufferGeometry( 40, 40, 40 );
@@ -460,6 +427,10 @@ class ModelLibrary
       }
     }
   }
+
+  var toggleSunCircling = function(){
+      sunCircling = !sunCircling;
+  };
 
 
   class houseLightManager
@@ -1177,8 +1148,10 @@ class ModelLibrary
 
   var MyUpdateLoop = function() {
     requestAnimationFrame(MyUpdateLoop);
-    environment.time +=.5;
-    environment.update();
+    if (sunCircling) {
+        environment.time += effectController.sunSpeed;
+        environment.update();
+    }
     //console.log(environment.time);
     renderer.render(scene, camera);
   };
